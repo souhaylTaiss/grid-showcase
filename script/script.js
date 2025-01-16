5e1593427826f3f20a441743efc876f6c222e999
Souhayl Taiss <frontend.souhayl@gmail.com>
2025-01-16 21:27:42 +0100

    add new popup and an close img with thier code and style in js and css file

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@
  * Date: 16-01-2025.
  */
 
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
   const container = document.querySelector(".container");
 
   // Create 50 boxes with a header and a paragraph
@@ -28,4 +28,60 @@ document.addEventListener("DOMContentLoaded", function() {
   const img = document.createElement("img");
   img.src = "./assets/images/hot-air-balloons-6757939.jpg";
   container.appendChild(img);
+
+
+// });
+const box = document.querySelectorAll(".container div");
+
+box.forEach((div) => {
+  div.addEventListener("click", () => {
+    let overlay = document.createElement("div"),
+      popupBox = document.createElement("div"),
+      closeButton = document.createElement("img"),
+      popupImage = document.createElement("img");
+
+    overlay.className = "popup-overlay";
+    popupBox.className = "popup-box";
+    closeButton.className = "close-button";
+    popupImage.src = "./assets/images/hot-air-balloons-6757939.jpg";
+    closeButton.src = "./assets/images/clear.png";
+    document.body.append(overlay);
+    popupBox.append(closeButton);
+    popupBox.append(popupImage);
+    document.body.append(popupBox);
+
+    setTimeout(() => {
+      popupBox.style.top = "50%";
+    }, 800);
+
+    popupBox.addEventListener("transitionend", showImage);
+    closeButton.addEventListener("click", hideImage);
+
+    function showImage() {
+      popupBox.classList.add("show");
+      popupBox.style.transform = "translate(-50%,-50%)";
+      closeButton.style.top = "-44px";
+      popupImage.style.opacity = 1;
+      setTimeout(() => {
+        popupBox.style.height = popupBox.clientHeight + "px";
+      }, 2000);
+      popupBox.removeEventListener("transitionend", showImage);
+    }
+
+    function hideImage() {
+      popupImage.style.opacity = 0;
+      popupBox.style.height = 44 + "px";
+      closeButton.style.top = "0";
+      popupBox.classList.remove("show");
+      popupBox.addEventListener("transitionend", removeImage);
+    }
+
+    function removeImage() {
+      popupBox.style.top = "-50%";
+      setTimeout(() => {
+        overlay.remove();
+        popupBox.remove();
+      }, 1000);
+    }
+  });
 });
